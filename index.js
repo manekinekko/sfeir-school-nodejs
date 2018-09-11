0af50b0b879873446ecb2cd3ca5df29b3bf7e566
Siegfried Ehret <siegfried@ehret.me>
2018-09-11 03:41:07 +0200

    :gift: step-12 solution
    tagzytout: project-12-solution

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const {
   MONGO_INITDB_ROOT_USERNAME = "root",
   MONGO_INITDB_DATABASE = "schoolsdb",
   MONGO_INITDB_DATABASE_USERS = "admin",
+  DATABASE_HOST = "localhost",
+  DATABASE_PORT = 27017,
   PORT = 3000
 } = process.env;
 
-const url = `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@localhost:27017/${MONGO_INITDB_DATABASE_USERS}`;
+const url = `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${MONGO_INITDB_DATABASE_USERS}`;
 
 process.on("uncaughtException", err => {
   log.error({ err }, "Got an uncaught exception");
@@ -23,18 +25,31 @@ process.on("unhandledRejection", err => {
   process.exit(1);
 });
 
-MongoClient.connect(
-  url,
-  { useNewUrlParser: true },
-  function(err, client) {
-    if (err) {
-      log.error({ err }, "Error while connecting to DB");
+const connect = () => {
+  MongoClient.connect(
+    url,
+    { useNewUrlParser: true, connectTimeoutMS: 1000 },
+    function(err, client) {
+      if (err || !client) {
+        log.error({ err }, "Error while connecting to DB");
+      } else {
+        clearInterval(interval);
+        clearTimeout(timeout);
 
-      process.exit(1);
+        const db = client.db(MONGO_INITDB_DATABASE);
+
+        app(db).listen(PORT, () => log.info(`App listening on port ${PORT}!`));
+      }
     }
+  );
+};
 
-    const db = client.db(MONGO_INITDB_DATABASE);
+const interval = setInterval(connect, 3000);
 
-    app(db).listen(PORT, () => log.info(`App listening on port ${PORT}!`));
-  }
-);
+const timeout = setTimeout(() => {
+  clearInterval(interval);
+
+  log.error("Failed to connect to database!");
+
+  process.exit(1);
+}, 10000);
