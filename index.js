414497361d617bdc1bfee06a6a6ec795ea424a90
Siegfried Ehret <siegfried@ehret.me>
2018-09-11 03:18:17 +0200

    :gift: step-09 solution
    tagzytout: project-09-solution

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const MongoClient = require("mongodb").MongoClient;
 
 const app = require("./lib/app");
+const log = require("./lib/logger")();
 
 const {
   MONGO_INITDB_ROOT_PASSWORD = "example",
@@ -12,18 +13,28 @@ const {
 
 const url = `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@localhost:27017/${MONGO_INITDB_DATABASE_USERS}`;
 
+process.on("uncaughtException", err => {
+  log.error({ err }, "Got an uncaught exception");
+  process.exit(1);
+});
+
+process.on("unhandledRejection", err => {
+  log.error({ err }, "Got an unhandled rejection");
+  process.exit(1);
+});
+
 MongoClient.connect(
   url,
   { useNewUrlParser: true },
   function(err, client) {
-    if (err || !client) {
-      console.log("Error while connecting to DB", err);
+    if (err) {
+      log.error({ err }, "Error while connecting to DB");
 
       process.exit(1);
     }
 
     const db = client.db(MONGO_INITDB_DATABASE);
 
-    app(db).listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+    app(db).listen(PORT, () => log.info(`App listening on port ${PORT}!`));
   }
 );
